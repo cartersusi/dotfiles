7b878dac357091d5c88d00895187838848baf19e
Carter Susi <133709987+carter4299@users.noreply.github.com>
2024-04-03 12:19:48 -0500

    Add dosfstools

--- linux.sh
+++ linux.sh
@@ -2,7 +2,7 @@
 
 CUR_DIR=$(pwd)
 
-pacman_pkgs="firefox gimp inkscape gnome-calculator discord docker neovim nodejs npm obs-studio btop go rustup nvidia-dkms opencl-nvidia nvidia-utils nvidia-settings unzip fish"
+pacman_pkgs="firefox gimp inkscape gnome-calculator discord docker neovim nodejs npm obs-studio btop go rustup nvidia-dkms opencl-nvidia nvidia-utils nvidia-settings unzip fish dosfstools"
 
 AUR_pkgs=(
     "gwe"
